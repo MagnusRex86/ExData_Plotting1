a60f210e6740aac34ca921edcff6b7f37fa9daa5
MagnusRex86 <39067264+MagnusRex86@users.noreply.github.com>
2018-08-21 21:18:36 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -4,7 +4,7 @@ TimeDate<-strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 data<-cbind(data, TimeDate)
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 
-##Plot1
+##Plot2
 subsetdata <- subset(data, Date == "2007-02-01" | Date =="2007-02-02")
 png("plot2.png", width=480, height=480)
 with(subsetdata,plot(TimeDate, Global_active_power, type="l",xlab="Day", ylab="Global Active Power (kilowatts)"))
